0c2cbcbf1298ce7628883b6d6df55f325245f7f3
Павел Топорков <ptrk@MacBook-Pro-Pavel.local>
2020-12-05 12:20:45 +0300

    remove bad files

--- lab01/data/main.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("./nation1.csv", "r") as fr, open("./nation.csv", "w") as fw:
-    reader = csv.reader(fr)
-    writer = csv.writer(fw)
-
-    for row in reader:
-        curRow = [row[0]] + list(map(lambda el: el.replace("NA", ""), row[1:]))
-        writer.writerow(curRow)
